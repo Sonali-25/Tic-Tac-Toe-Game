d88ab160c6d048dc6116a93dddd022541d3dc5b9
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-05-29 14:03:55 +0530

    Added=UC1

--- src/com/magic/game/TicTacToe.java
+++ src/com/magic/game/TicTacToe.java
@@ -1,7 +1,19 @@
 package com.magic.game;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("welcome To Tic-Tac-Toe");
+    public static final int boardSize = 10;
+    char computer = ' ';
+    char player = ' ';
+    public static char board[] = new char[boardSize];
+
+    public static void board(){
+        for(int i = 0; i < boardSize; i++)
+            board[i] = ' ';
+        System.out.println("Welcome to TicTacToe Game!");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Welcome To Tic-Tac_Toe Game");
+        board();
     }
 }
