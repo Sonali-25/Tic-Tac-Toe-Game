f46557c365accea35d11f9e954f4e0f876172497
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-05-29 14:22:11 +0530

    Added=UC3

--- src/com/magic/game/TicTacToe.java
+++ src/com/magic/game/TicTacToe.java
@@ -1,10 +1,12 @@
 package com.magic.game;
 import java.util.*;
+
 public class TicTacToe{
     private static String player_choice;
     private static String computer_choice;
-    public char[] board;
-    public static char[] Board() {
+    private static char [][] boardGame = new char [3][3];
+    public static char[] board;
+    public static char[] board() {
         char[] board = new char[10];
         for(int i=1; i<board.length; i++) {
             board[i]=' ';
@@ -25,14 +27,26 @@ public class TicTacToe{
         }
     }
 
+    public static void showBoard(){
+        System.out.println("\n "+boardGame[0][0]+" | "+boardGame[0][1]+" | "+boardGame[0][2]+" ");
+        System.out.println("-----------");
+        System.out.println(" "+boardGame[1][0]+" | "+boardGame[1][1]+" | "+boardGame[1][2]+" ");
+        System.out.println("-----------");
+        System.out.println(" "+boardGame[2][0]+" | "+boardGame[2][1]+" | "+boardGame[2][2]+" ");
+
+
+    }
+
     public static void main(String[] args) {
 
         Scanner sc= new Scanner(System.in);
         System.out.println("Choice letter X or 0 for player");
         player_choice = sc.nextLine();
         System.out.println("Welcome to TicTacToe Game");
-        char[] board = Board();
+        char[] board = board();
         chooseLetter();
+        showBoard();
+
         sc.close();
     }
 }
